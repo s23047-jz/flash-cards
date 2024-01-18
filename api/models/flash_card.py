eb51a5b52615ef57444acc51b84b0c000ddfe5c3
Oliwier Kossak <s22018@pjwstk.edu.pl>
2024-01-18 11:19:22 +0100

    dodanie modelu flash card i deck wraz z stworzonymi relacjami

---
+++ api/models/flash_card.py
@@ -0,0 +1,32 @@
+import uuid
+
+from sqlalchemy.orm import relationship
+
+from deck_of_flash_cards import Deck
+
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+    Boolean,
+    Integer,
+    ForeignKey,
+    UUID,
+    Text,
+    func
+)
+
+from api.models import Base
+
+
+class FlashCard(Base):
+    __tablename__ = 'flash_card'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    deck_id = Column(UUID(as_uuid=True), ForeignKey("deck.id"))
+    password = Column(String(30), nullable=False)
+    username = Column(Text, nullable=False)
+    deck_relationship = relationship("Deck", back_populates='flash_card_relationship')
+
+    class Config:
+        orm_mode = True
