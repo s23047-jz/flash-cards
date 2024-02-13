c06bd793aaa654620ca5e50c76565ae300f85293
Jusiiek <jakub.zurawski@outlook.com>
2024-02-13 20:33:19 +0100

    Zmieniono nazwy modeli i tablic dla tokenow

--- api/models/token.py
+++ api/models/token.py
@@ -1,14 +1,18 @@
+import uuid
+
 from sqlalchemy import (
     Column,
-    String
+    String,
+    UUID
 )
 
 from models import Base
 
 
 class Token(Base):
-    __tablename__ = "token"
+    __tablename__ = "tokens"
 
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     access_token = Column(String(300), nullable=False, unique=True)
     token_type = Column(String(10), nullable=False, unique=True, default="Bearer")
     user_email = Column(String(50), nullable=False, unique=True)
@@ -17,7 +21,8 @@ class Token(Base):
         orm_mode = True
 
 
-class BlackToken(Base):
-    __tablename__ = "black_token"
+class Blacklist_Tokens(Base):
+    __tablename__ = "blacklist_tokens"
 
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     token = Column(String(300), nullable=False, unique=True)
