38a12ddd9b1a3c7dc6dbd88f651a3c73716922c5
s22018-pj-Oliwier-Kossak <s22018@pjwstk.edu.pl>
2024-02-18 13:00:52 +0100

    poprawa modelu flash card, dodanie .env do git ignore

--- api/models/flash_card.py
+++ api/models/flash_card.py
@@ -20,8 +20,8 @@ class FlashCard(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     deck_id = Column(UUID(as_uuid=True), ForeignKey("deck.id"))
-    password = Column(String(30), nullable=False)
-    username = Column(Text, nullable=False)
+    card_title = Column(String(30), nullable=False)
+    card_text = Column(Text, nullable=False)
     deck_relationship = relationship("Deck", back_populates='flash_card_relationship')
 
     class Config:
