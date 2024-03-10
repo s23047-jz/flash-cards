97b43dad47bbd8e9ab187501bcb9cd66cb81d99e
s22018-pj-Oliwier-Kossak <s22018@pjwstk.edu.pl>
2024-03-10 20:29:31 +0100

    dodanie endpointów dla fiszek poprawa metody get dla get deck by id

--- api/flash_cards_api/endpoints/decs.py
+++ api/flash_cards_api/endpoints/decs.py
@@ -30,22 +30,9 @@ class DeckUpdate(DeckCreate):
     downloads: int
 
 
-# @router.get("/flash_card/{flash_card_id}", status_code=status.HTTP_200_OK)
-# async def read_flash_card_by_id(flash_card_id: str, db: Session = Depends(get_db)):
-#     """Return flash card by id"""
-#     flash_card = db.query(FlashCard).filter(FlashCard.id == flash_card_id).first()
-#
-#     if flash_card is not None:
-#         return flash_card
-#     raise HTTPException(status_code=404, detail="Flash card not found")
-#
-
-# class DeckDetailsResponse(BaseModel):
-#     pass
-
 @router.get("/{deck_id}", status_code=status.HTTP_200_OK)
 async def read_deck_by_id(
-        deck_id: str,
+        deck_id: uuid.UUID,
         db: Session = Depends(get_db)
 ):
     """Return deck by id"""
@@ -58,8 +45,8 @@ async def read_deck_by_id(
 
 @router.post("/create_deck", status_code=status.HTTP_201_CREATED)
 async def create_deck(
-    deck: DeckCreate,
-    db: Session = Depends(get_db)
+        deck: DeckCreate,
+        db: Session = Depends(get_db)
 ):
     """Create a new deck"""
     deck_model = Deck(**deck.dict())
@@ -71,9 +58,9 @@ async def create_deck(
 
 @router.put("/update_deck/{deck_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_deck(
-    deck: DeckUpdate,
-    deck_id: uuid.UUID,
-    db: Session = Depends(get_db)
+        deck: DeckUpdate,
+        deck_id: uuid.UUID,
+        db: Session = Depends(get_db)
 ):
     deck_model = db.query(Deck).filter(Deck.id == deck_id).first()
     if deck_model is None:
@@ -90,8 +77,8 @@ async def update_deck(
 
 @router.delete("/delete_deck/{delete_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_deck(
-    delete_id: uuid.UUID,
-    db: Session = Depends(get_db)
+        delete_id: uuid.UUID,
+        db: Session = Depends(get_db)
 ):
     """Delete deck"""
     deck = db.query(Deck).filter(Deck.id == delete_id).first()
