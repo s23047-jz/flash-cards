74201218e5231e5929923042a4d64a29aca53716
Jusiiek <jakub.zurawski@outlook.com>
2024-03-15 20:58:29 +0100

    Created request method, #FC-33

--- mobile/src/interfaces/auth.ts
+++ mobile/src/interfaces/auth.ts
@@ -1,23 +1,19 @@
 export interface TokenInterface {
-    access_token: string
-    token_type: string
+    access_token?: string
+    token_type?: string
 }
 
 
 export interface UserInterface {
-    id: string
-    email: string
-    username: string
-    created_at: Date
-    updated_at: Date
-    active: boolean
-    role: string
-    is_superuser: boolean
+    id?: string
+    email?: string
+    username?: string
+    created_at?: Date
+    updated_at?: Date
+    active?: boolean
+    role?: string
+    is_superuser?: boolean
 }
-
-
-export type TokenDataType = TokenInterface | {};
-export type UserDataType = UserInterface | {};
 
 
 export interface AuthInterface {
---
+++ mobile/src/utils/request.ts
@@ -0,0 +1,83 @@
+import ActiveUser from "../services/user";
+// TODO add querystring
+
+function decode(obj: Object): string {
+    const keys = Object.keys(obj);
+    const keyValuePairs = keys.map(key => {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]);
+    });
+    return keyValuePairs.join('&');
+}
+
+export const redirectIfNotAuthenticated = (res: Response) => {
+    if (res.status === 401) {
+        const user = new ActiveUser();
+        user.clean();
+        // TODO add redirect to login page
+    }
+    if (res.status === 403) {
+        // TODO add redirect to home view for logged
+    }
+}
+
+export async function request({
+    url,
+    query,
+    headers = {},
+    method = "GET",
+    body,
+    formData,
+    skipRedirect = false
+}: {
+    url: string;
+    query?: Object;
+    headers?: Record<string, string>;
+    method?: string;
+    body?: Object;
+    formData?: FormData;
+    skipRedirect?: boolean;
+})
+{
+    const methodLower: string = method.toLowerCase()
+    const jsonMethods: string[] = ["post", "put", "patch", "delete"]
+
+    if (body) {
+        body = JSON.stringify(body);
+    }
+    if (formData) {
+        body = formData
+    }
+
+    const user = new ActiveUser();
+    const token = user.getAuthorization();
+
+    if (token) headers.Authorization = token
+    if (query) url = `${url}?${decode(query)}`
+
+    const res: Response = await fetch(url, {
+        method,
+        headers,
+        body
+    })
+
+    if (!skipRedirect) {
+        redirectIfNotAuthenticated(res)
+    }
+
+    const contentType: string | null = res.headers.get('content-type')
+    if (contentType && contentType !== 'application/json') {
+        return {
+            res,
+            data: {},
+            headers: res.headers
+        }
+    }
+
+    const data = res.json();
+
+    return {
+        res,
+        data,
+        headers: res.headers
+    }
+};
