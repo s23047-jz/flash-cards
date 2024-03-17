338c9244b3a4c52169ee05eb4e2e9865e4d04d09
s23638-pj <s23638@pjwstk.edu.pl>
2024-03-18 00:11:50 +0100

    First commit, Login Page

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import SignInSide from "./views/SignIn.tsx"; // Importowanie komponentu SignInSide
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignInSide /> {/* Dodanie komponentu SignInSide */}
     </div>
   );
 }
---
+++ web/src/views/SignIn.tsx
@@ -0,0 +1,90 @@
+// @ts-ignore
+import React from 'react';
+// @ts-ignore
+import logo from '../assets/images/logo.png';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Paper, Box, Grid, Typography, Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+const theme = createTheme();
+
+const LoginPage = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Paper
+          elevation={6}
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: 4,
+            borderRadius: '30px', // Tutaj ustawiasz pożądany poziom zaokrąglenia rogów
+          }}
+        >
+          <Avatar sx={{ m: 1, width: 200, height: 200 }} src={logo} />
+          <Typography component="h1" variant="h4">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username or Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Log In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  {"Forgot password?"}
+                </Link>
+              </Grid>
+              <Grid item xs>
+                <Link href="RegistrationPage.tsx" variant="body2">
+                  {"Don't have an account?"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default LoginPage;
