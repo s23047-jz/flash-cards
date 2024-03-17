2a746eb26aef021dd8c960b7c5cfc41f76ed5715
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-03-17 18:10:25 +0100

    Updated auth service, #FC-34

--- mobile/src/services/auth.ts
+++ mobile/src/services/auth.ts
@@ -1,20 +1,23 @@
 import {BASE_API} from "./config";
 import {request} from "../utils/request";
+import {ActiveUser} from "./user";
 
 export const AUTH_ENDPOINTS = {
-    login: `${BASE_API}/auth/login/`,
-    register: `${BASE_API}/auth/register/`,
-    logout: `${BASE_API}/auth/logout/`,
+    login: `${BASE_API}/api/auth/login/`,
+    register: `${BASE_API}/api/auth/register/`,
+    logout: `${BASE_API}/api/auth/logout/`,
 }
 
-export class AuthService {
-
+class Auth {
     constructor() {}
-    public async login(body: Object) {
-        const data = await request({
+    public async login(body: object) {
+        const { data } = await request({
             url: AUTH_ENDPOINTS.login,
-            body: body
+            method: 'POST',
+            body
         })
-        console.log(data)
+        ActiveUser.set(data)
     }
 }
+
+export const AuthService = new Auth();
