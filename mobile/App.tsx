01fa57f5e5c2d13e858668da70482f71a6afefc5
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-03-19 23:22:38 +0100

    Imported AuthNavigator to App , #FC-46

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useState } from "react";
-
-import ForgotPassScreen from "./src/screens/ForgotPassScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import SignUpScreen from "./src/screens/SignUpScreen";
-
-const AuthStack = createNativeStackNavigator();
-const MainStack = createNativeStackNavigator();
 
 import AuthNavigator from "./src/navigators/AuthNavigator";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const LoginMode = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <NavigationContainer>
       <AuthNavigator />
