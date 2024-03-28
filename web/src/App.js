4fa9cbc0c825b9dd74489f0a2788eef7a3348cc3
s23638-pj <s23638@pjwstk.edu.pl>
2024-03-28 21:17:50 +0100

    Logowanie #3

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,22 @@
-import './App.scss';
-import SignInSide from "./views/SignIn.tsx"; // Importowanie komponentu SignInSide
+import React from 'react';
+import './styles/App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './views/SignIn.tsx';
+import Registration from './views/Registration.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <SignInSide /> {/* Dodanie komponentu SignInSide */}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/registration" element={<Registration />} />
+          {/* Możesz dodać więcej ścieżek i komponentów tutaj */}
+          {/* Domyślna ścieżka/strona główna */}
+          {/*<Route path="/" element={<div>Strona główna</div>} />*/}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ web/src/services/auth.ts
@@ -0,0 +1,25 @@
+import { BASE_API } from './config';
+// @ts-ignore
+import { request } from '../utils/request';
+import { ActiveUser } from './user';
+
+export const AUTH_ENDPOINTS = {
+  login: `${BASE_API}/api/auth/login/`,
+  register: `${BASE_API}/api/auth/register/`,
+  logout: `${BASE_API}/api/auth/logout/`,
+};
+
+class Auth {
+  constructor() {}
+
+  public async login(body: object) {
+    // @ts-ignore
+    const { data } = await request({
+      url: AUTH_ENDPOINTS.login,
+      method: 'POST', body
+    });
+    ActiveUser.set(data);
+  }
+}
+
+export const AuthService = new Auth();
