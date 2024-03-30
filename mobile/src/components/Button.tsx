e4a9f4ffd38ca1023b6fdf55398c488aac04fe5c
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-03-30 11:16:54 +0100

    Created button component, #FC-45

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -5,7 +5,7 @@ import { LayoutsInterface } from "../interfaces/layouts";
 
 const Button: React.FC<LayoutsInterface> = ({ className, children }) => {
     return (
-        <TouchableOpacity className={`border-sky-600 border-2 rounded-3xl object-scale-down ${className}`}>
+        <TouchableOpacity className={`bg-cyan-400 border-sky-600 border-2 rounded-md object-scale-down ${className}`}>
             { children }
         </TouchableOpacity>
     )
