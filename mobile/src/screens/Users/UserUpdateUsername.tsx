80f53faa58791e657b2511b7644a73142205a20b
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-04-07 17:18:14 +0200

    Create user update username screen, #FC-54

---
+++ mobile/src/screens/Users/UserUpdateUsername.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+
+const UserUpdateUsername = () => {
+    return (
+        <View>
+            <Text>
+                Some text user update email
+            </Text>
+        </View>
+    )
+};
+
+export default UserUpdateUsername;
