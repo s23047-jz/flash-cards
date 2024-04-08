9965c576901abd51cfa9b35164880bb5e329e85d
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-04-08 15:32:04 +0200

    created users service, #FC-54

---
+++ mobile/src/services/users.ts
@@ -0,0 +1,22 @@
+import {BASE_API} from "./config";
+import {request} from "../utils/request";
+import { NavigationProp } from "@react-navigation/native";
+
+export const USERS_ENDPOINTS = {
+    me: `${BASE_API}/api/users/me/`,
+};
+
+class Users {
+    constructor() {}
+
+    public async updateMe(body: object, navigation: NavigationProp<any>) {
+        return await request({
+            url: USERS_ENDPOINTS.me,
+            method: 'PUT',
+            body,
+            navigation
+        })
+    }
+};
+
+export const UsersService = new Users();
