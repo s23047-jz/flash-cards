f59019c4ebf9d0e3b50500f32980e4eebff570b1
klimono <s18504@pjwstk.edu.pl>
2024-04-09 18:31:39 +0200

    FC53-widok-my-decks

--- mobile/src/navigators/BottomTabNavigator.tsx
+++ mobile/src/navigators/BottomTabNavigator.tsx
@@ -4,12 +4,14 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen, UserPanelScreen } from "../screens";
 import UserPanelNavigator from "./UserPanelNavigator";
 import {ROUTES} from "../constants"
+import MyDecks from "../screens/PrivateDecks/MyDecks";
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
     return (
         <Tab.Navigator initialRouteName={ROUTES.HOME} screenOptions={{headerShown: false}}>
+            <Tab.Screen name={ROUTES.MYDECKS} component={MyDecks} />
             <Tab.Screen name={ROUTES.HOME} component={HomeScreen} />
             <Tab.Screen name={ROUTES.USER} component={UserPanelNavigator} />
         </Tab.Navigator>
