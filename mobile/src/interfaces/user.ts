8041737ebe731e767e28ee8a113108abe3e131bd
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-04-21 16:43:55 +0200

    Updated user and components interfaces, #FC-70

--- mobile/src/interfaces/user.ts
+++ mobile/src/interfaces/user.ts
@@ -2,5 +2,6 @@ export interface UpdateUserInterface {
     username?: string,
     email?: string,
     password?: string,
-    re_password?: string
+    re_password?: string,
+    current_password?: string
 };
