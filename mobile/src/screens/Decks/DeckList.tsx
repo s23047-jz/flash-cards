4746d475af4b50d68a11b5bc5bf893456d2a1652
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-05-12 10:51:37 +0200

    Created a deck list screen, #FC-80

---
+++ mobile/src/screens/Decks/DeckList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import { ScreenProps } from "../../interfaces/screen";
+
+
+const DeckList: React.FC<ScreenProps> = ({ navigation }) => {
+    return (
+        <Text>hehehe</Text>
+    )
+};
+
+export default DeckList;
