cb9116fba55904a2f0a6082ed607dd530f955ab8
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-05-16 20:21:29 +0200

    Added account deletion confirmation modal, #FC-79

--- mobile/src/screens/Users/UserPanelScreen.tsx
+++ mobile/src/screens/Users/UserPanelScreen.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import { View, Image, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, Image, Text, ScrollView, TouchableOpacity } from "react-native";
 import { ScreenProps } from "../../interfaces/screen";
 
-import {Row, Col, Button} from "../../components";
+import { Row, Col, Button, CModal } from "../../components";
 
 import LOGO from "../../assets/images/logo.png"
 
 import { AuthService } from "../../services/auth";
 import Routes from "../../constants/routes";
 import DarkMode from "../../components/DarkMode";
+import { ROUTES } from "../../constants";
 
 const UserPanelScreen: React.FC<ScreenProps> = ({navigation, route}) => {
 
     const { userData, getUserData } = route.params;
+
+    const [showModal, setShowModal] = useState(false);
 
     const options = [
         {
@@ -43,26 +46,67 @@ const UserPanelScreen: React.FC<ScreenProps> = ({navigation, route}) => {
                     label: "Change password",
                     to: Routes.USER_UPDATE,
                     params: { updateField: 'password', getUserData }
-                },
-                {
-                    label: "Delete account",
-                    to: ""
                 }
             ]
         }
     ]
+
+    const handleDelete = () => {
+        setShowModal(false);
+        navigation.navigate(ROUTES.USER_DELETE);
+    }
 
     const logout = async () => {
         console.log("LOGIN OUT")
         await AuthService.logout(navigation);
     }
 
+    const getDeleteModal = () => {
+        return (
+            <CModal
+                visible={showModal}
+                animationType={'fade'}
+                transparent={true}
+            >
+                <View className={'bg-sky-500 dark:bg-blue-900 w-full p-4 rounded-xl'}>
+                    <Row className={'w-full mt-5'}>
+                        <Col className={'w-full mb-4 text-center'}>
+                            <Text className={'text-xl font-bold ml-auto mr-auto text-white'}>
+                                Delete User Account
+                            </Text>
+                        </Col>
+                        <Col className={'w-full mb-4 text-center'}>
+                            <Text className={'text-lg ml-auto mr-auto text-center text-white'}>
+                                You will be redirected to the user deletion form.
+                            </Text>
+                        </Col>
+                        <Col className={'w-full mt-4'}>
+                            <Button
+                                className={'p-3 w-52 text-center mr-auto ml-auto'}
+                                onPress={handleDelete}
+                            >
+                                <Text className={'text-lg ml-auto mr-auto font-bold'}>
+                                    Continue
+                                </Text>
+                            </Button>
+                        </Col>
+                        <Col className={'w-full mt-4'}>
+                            <TouchableOpacity className={'p-1 w-52 text-center mr-auto ml-auto'} onPress={() => setShowModal(false)} disabled={false}>
+                                <Text className={'text-lg ml-auto mr-auto font-bold'}>
+                                    Cancel
+                                </Text>
+                            </TouchableOpacity>
+                        </Col>
+                    </Row>
+                </View>
+            </CModal>
+        )
+    }
+
 
     return (
-
-
         <View className="flex h-screen w-full bg-sky-500 dark:bg-blue-900">
-
+            {getDeleteModal()}
             <ScrollView className="flex flex-container w-full mt-20 mb-5">
                 <Row className="w-full">
                     <Col className='w-full'>
@@ -109,14 +153,18 @@ const UserPanelScreen: React.FC<ScreenProps> = ({navigation, route}) => {
 
                             <Row className='w-full'/>
                             <Row className='w-full mt-1 mb-1'>
-
-
-                                    <DarkMode/>
-
+                                <DarkMode/>
                             </Row>
-
                             <Row className='w-full mt-1 mb-1'>
-
+                                <Button className='w-full p-2' onPress={() => setShowModal(true)}>
+                                    <Row className='w-full'>
+                                        <Text className='mx-5 font-bold text-xl'>
+                                            Delete account
+                                        </Text>
+                                    </Row>
+                                </Button>
+                            </Row>
+                            <Row className='w-full mt-1 mb-1'>
                                 <Button className='w-full p-2' onPress={logout}>
                                     <Row className='w-full'>
                                         <Text className='mx-5 font-bold text-xl'>
@@ -125,13 +173,9 @@ const UserPanelScreen: React.FC<ScreenProps> = ({navigation, route}) => {
                                     </Row>
                                 </Button>
                             </Row>
-
                         </Col>
-
                     </Col>
-
                 </Row>
-
             </ScrollView>
         </View>
     )
