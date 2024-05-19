5595e46ba64b3e4194eb4fb1ce2e4abed37ec4ce
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-05-19 17:20:00 +0200

    Created new interface for users stats, #FC-97

--- mobile/src/interfaces/user.ts
+++ mobile/src/interfaces/user.ts
@@ -5,3 +5,12 @@ export interface UpdateUserInterface {
     re_password?: string,
     current_password?: string
 };
+
+export interface UserStatsInterface {
+    id?: string,
+    username?: string,
+    avatar?: string,
+    ranking?: number,
+    created_decks?: number,
+    public_decks?: number
+}
