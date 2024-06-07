f708ae2bfbc50dc39f46511ca83cd02852bf6344
OliwierKossak <oliwier.kossak109@gmail.com>
2024-06-07 13:54:20 +0200

    poprawnie okienka instrukcji

--- web/src/components/alert/VoiceControlInstruction.tsx
+++ web/src/components/alert/VoiceControlInstruction.tsx
@@ -8,13 +8,13 @@ const VoiceControlInstruction = ({onClose}) => {
           <p>Voice control instructions.</p>
           <p>After clicking "Start Listening", the application starts listening.</p>
           <p>Voice commands to operate the deck:</p>
-          <p>Show next flashcard - go to the next flashcard</p>
-          <p>Show previous flashcard - go to the previous flashcard</p>
-          <p>Rotate flashcard - rotate the flashcard</p>
-          <p>Read the text - reading the content of the flashcard</p>
-          <p>Stop control - turning off the content of the flashcard</p>
+          <p className="commands">Show next flashcard - go to the next flashcard</p>
+          <p className="commands">Show previous flashcard - go to the previous flashcard</p>
+          <p className="commands"> Rotate flashcard - rotate the flashcard</p>
+          <p className="commands">Read the text - reading the content of the flashcard</p>
+          <p className="commands">Stop control - turning off the content of the flashcard</p>
           <p>The user does not have to carefully pronounce these sentences for the actions to be performed,</p>
-          <p> the application will match the spoken sentences to the above commands.</p>
+          <p>the application will match the spoken sentences to the above commands.</p>
 
     <button className="closeButtonVoiceControlInstruction" onClick={onClose}>Close</button>
       </div>
