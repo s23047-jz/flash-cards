1eee025b115bf1b2b5d0179ad09c2203c960c4d6
klimono <s18504@pjwstk.edu.pl>
2024-06-18 14:24:18 +0200

    poprawki

--- mobile/src/screens/ChooseDecksType/EditDeck.tsx
+++ mobile/src/screens/ChooseDecksType/EditDeck.tsx
@@ -1,37 +1,41 @@
-import React, {useState} from 'react';
-import {View, Text, TextInput} from 'react-native';
-import {ScreenProps} from "../../interfaces/screen";
-import {MaterialCommunityIcons} from "@expo/vector-icons";
-import {Button} from "../../components";
-import {InputValidator} from "../../components/Validator/InputValidator";
-import {DecksService} from "../../services/decks";
-import {ROUTES} from "../../constants";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+
+import { Button } from "../../components";
+import { InputValidator } from "../../components/Validator/InputValidator";
+import { ROUTES } from "../../constants";
+import { ScreenProps } from "../../interfaces/screen";
+import { DecksService } from "../../services/decks";
 const EditDeck: React.FC<ScreenProps> = ({ navigation, route }) => {
   useState();
 
   const { deck } = route.params;
-  console.log(deck)
+  console.log(deck);
   const [title, setTitle] = useState(deck.title || "");
   const [category, setCategory] = useState(deck.deck_category || "");
-  
+
   const handleEdit = async () => {
     if (InputValidator("deck", title) && InputValidator("deck", category)) {
       const deck_data = {
-        title: title,
+        title,
         deck_category: category,
       };
       try {
-        await DecksService.update_deck_title_category(deck.id, deck_data, navigation);
+        await DecksService.update_deck_title_category(
+          deck.id,
+          deck_data,
+          navigation,
+        );
         navigation.goBack();
-        console.log("Editing deck...");
       } catch (error) {
-        alert("Nie udało się edytować decku");
+        alert("Update deck failed");
       }
     } else {
       alert("Flashcards fields must not be empty.");
     }
-  }
-  
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-sky-500 dark:bg-blue-900 placeholder-gray-400">
       <Text className="text-white font-extrabold animate-bounce scale-150 absolute top-16 right-10">
@@ -45,7 +49,7 @@ const EditDeck: React.FC<ScreenProps> = ({ navigation, route }) => {
           color="white"
         />
       </View>
-      
+
       <View className="m-3">
         <Text className="text-base mb-3 left-12 text-white font-bold scale-125">
           Title:
@@ -57,7 +61,7 @@ const EditDeck: React.FC<ScreenProps> = ({ navigation, route }) => {
           onChangeText={setTitle}
         />
       </View>
-      
+
       <View className="m-3">
         <Text className="text-base mb-3 left-12 text-white font-bold scale-125">
           Category:
@@ -69,7 +73,7 @@ const EditDeck: React.FC<ScreenProps> = ({ navigation, route }) => {
           onChangeText={setCategory}
         />
       </View>
-      
+
       <View className="flex-row top-6">
         <Button
           onPress={() => navigation.goBack()}
