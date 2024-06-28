9ca17263083711646ce995f096ea04963762e795
klimono <s18504@pjwstk.edu.pl>
2024-06-28 20:09:54 +0200

    obrona

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,9 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthNavigator } from "./src/navigators";
-
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+console.warn = () => {};
 
 export default function App() {
   return (
